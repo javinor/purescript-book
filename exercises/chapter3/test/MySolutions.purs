f6705722c2bbd6e94a40c706669a2fb3428a1c8e
Jonathan Avinor <javinor@gmail.com>
2020-09-03 11:01:27 +0300

    finish chapter 3

--- exercises/chapter3/test/MySolutions.purs
+++ exercises/chapter3/test/MySolutions.purs
@@ -1,5 +1,21 @@
 module Test.MySolutions where
 
 import Prelude
+import Data.AddressBook (AddressBook, Entry, findEntry)
+import Data.List (filter, head, nubBy)
+import Data.Maybe (Maybe, isJust)
 
--- Note to reader: Add your solutions to this file
+findEntryByStreet :: String -> AddressBook -> Maybe Entry
+findEntryByStreet street book = head $ filter filterEntry book
+  where
+  filterEntry :: Entry -> Boolean
+  filterEntry entry = entry.address.street == street
+
+isInBook :: String -> String -> AddressBook -> Boolean
+isInBook firstName lastName book = isJust $ findEntry firstName lastName book
+
+removeDuplicates :: AddressBook -> AddressBook
+removeDuplicates book = nubBy hasSameName book
+  where
+  hasSameName entry1 entry2 = 
+    entry1.firstName == entry2.firstName && entry1.lastName == entry2.lastName
